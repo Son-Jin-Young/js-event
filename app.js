ef7f2e70113ad752d3855acf8333ddd6b75c467c
es-sjyc <es-sjyc@smilegate.com>
2018-12-18 14:16:46 +0900

    초기 구조

---
+++ app.js
@@ -0,0 +1,9 @@
+var divs = document.querySelectorAll('div');
+
+divs.forEach(function(div) {
+	div.addEventListener('click', logEvent);
+});
+
+function logEvent(event) {
+	console.log(event.currentTarget.className);
+}
